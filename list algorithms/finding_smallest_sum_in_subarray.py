5d7cb45fa52aac5ef74554df00173d543de32ae0
loikx <vartleon854@gmail.com>
2021-05-04 22:35:57 +0300

    new branch

---
+++ list algorithms/finding_smallest_sum_in_subarray.py
@@ -0,0 +1,34 @@
+"""
+This file is contained algorithm that find max sum of elements of subarray.
+This algorithm is worked with O(n) asymptotic.
+"""
+
+
+def max_summary_of_subarray(array: list[float]) -> float:
+
+    # check input value type
+    if type(array) != list:
+        # throw the error
+        raise ValueError('Input is not a list type object')
+
+    # check the length of input array
+    if not array:
+        # if array is empty return 0
+        return 0
+
+    # initialize current sum variable
+    sum = 0
+
+    # initialize max sum variable
+    max_sum = array[0]
+    for i in array:
+        # add new element to sum
+        sum += i
+        # choose the max value of new sum last value and this element
+        max_sum = max(sum, max_sum, i)
+        # choose the max value of new sum and 0
+        # we need to do it because our current sum maybe negative
+        sum = max(sum, 0)
+
+    # return max_sum value
+    return max_sum
